c65f5ae9315146b4492650fbf2517caac08166f9
Sahib Singh <sahaibsingh001@live.com>
2024-12-11 02:07:28 +0530

    production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'service-worker.ts',
-      registerType: 'prompt',
-      injectRegister: 'auto',
-      manifest: false,
-      injectManifest: {
-        injectionPoint: undefined
-      }
-    })
-  ],
-  envPrefix: 'VITE_',
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   server: {
     port: 3001,
     host: true,
